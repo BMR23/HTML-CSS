a275c5b332ec97ceeace8e750f386eeddc898366
BMR23 <brenomelo161@hotmail.com>
2023-05-18 13:58:11 -0300

    sL

--- projetos/ladding-page/assets/js/script.js
+++ projetos/ladding-page/assets/js/script.js
@@ -2,7 +2,6 @@ let menuCss = document.getElementById('menu')
 let fechaMenuCelular = document.getElementById('fechaMenuCelular')
 let mensagem = document.getElementById('mensagem')
     mensagem = mensagem.addEventListener('click', enviarMensagem)
-
 let contador = 0
 let click = 0
 
@@ -11,6 +10,7 @@ function abrirMenu(){
     menu.style.display = 'block'
     fechaMenuCelular.style.display = 'block'
   }
+
 function fecharMenu(){
     if (contador == 1){
       contador = 0
@@ -18,34 +18,36 @@ function fecharMenu(){
       fechaMenuCelular.style.display = 'none'
     }
   }
-  function enviarMensagem(){
-  
-  if (click === 0){
-    click ++
-    var mensagemPadrao = document.querySelector('.mensagem-1')
-    var cLoader = document.querySelector('.c-loader')
-    var mensagemConcluido = document.querySelector('.concluido')
 
-    mensagemPadrao.classList.remove('aparece')
-    mensagemPadrao.classList.add('ocultar')
-    cLoader.classList.remove('ocultar')
-    cLoader.classList.add('aparece')
-
-    setTimeout(function(){
-      cLoader.classList.remove('aparece')
-      cLoader.classList.add('ocultar')
-    }, 3000)
-
-    setTimeout(function(){
-      mensagemConcluido.classList.remove('ocultar')
-      mensagemConcluido.classList.add('aparece')
-    }, 3000)
-    setTimeout(function(){
-      mensagemConcluido.classList.remove('aparece')
-      mensagemConcluido.classList.add('ocultar')
-      mensagemPadrao.classList.remove('ocultar')
-      mensagemPadrao.classList.add('aparece')
-      click = 0
-    }, 6000)
+function enviarMensagem(){
+  if (click != 0) {
+    return 0
   }
+  
+  click ++
+  var mensagemPadrao = document.querySelector('.mensagem-1')
+  var cLoader = document.querySelector('.c-loader')
+  var mensagemConcluido = document.querySelector('.concluido')
+
+  mensagemPadrao.classList.remove('aparece')
+  mensagemPadrao.classList.add('ocultar')
+  cLoader.classList.remove('ocultar')
+  cLoader.classList.add('aparece')
+
+  setTimeout(function(){
+    cLoader.classList.remove('aparece')
+    cLoader.classList.add('ocultar')
+  }, 3000)
+
+  setTimeout(function(){
+    mensagemConcluido.classList.remove('ocultar')
+    mensagemConcluido.classList.add('aparece')
+  }, 3000)
+  setTimeout(function(){
+    mensagemConcluido.classList.remove('aparece')
+    mensagemConcluido.classList.add('ocultar')
+    mensagemPadrao.classList.remove('ocultar')
+    mensagemPadrao.classList.add('aparece')
+    click = 0
+  }, 6000)
 }
