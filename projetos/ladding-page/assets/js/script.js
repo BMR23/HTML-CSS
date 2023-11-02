ebbddc236b940ec17a6cf8d5e2a3d3b6986d5dc4
BMR23 <brenomelo161@hotmail.com>
2023-11-02 11:55:59 -0300

    add JavaScript events

--- projetos/ladding-page/assets/js/script.js
+++ projetos/ladding-page/assets/js/script.js
@@ -1,30 +1,45 @@
 let menuCss = document.getElementById('menu')
 let fechaMenuCelular = document.getElementById('fechaMenuCelular')
 let mensagem = document.getElementById('mensagem')
-    mensagem = mensagem.addEventListener('click', enviarMensagem)
+mensagem = mensagem.addEventListener('click', enviarMensagem)
 let contador = 0
 let click = 0
 
-function abrirMenu(){
-    contador = 1
-    menu.style.display = 'block'
-    fechaMenuCelular.style.display = 'block'
-  }
+const bloquearRolagem = (event) => {
+  let body = document.getElementsByTagName('body')
+  body.classList.add('no-scroll')
+}
 
-function fecharMenu(){
-    if (contador == 1){
-      contador = 0
-      menu.style.display = 'none'
-      fechaMenuCelular.style.display = 'none'
-    }
-  }
+function abrirMenu() {
+  contador = 1
+  menu.style.display = 'block'
+  fechaMenuCelular.style.display = 'block'
+  let body = document.querySelector('#body')
+  body.classList.add('no-scroll')
+}
 
-function enviarMensagem(){
+function fecharMenu(event) {
+  var element = event.target;
+  console.log(element.tagName)
+
+  if (element.tagName !== "A") {
+    if (element.tagName === "NAV") return 0
+  }
+  if (contador == 1) {
+    contador = 0
+    menu.style.display = 'none'
+    fechaMenuCelular.style.display = 'none'
+    let body = document.querySelector('#body')
+    body.classList.remove('no-scroll')
+  }
+}
+
+function enviarMensagem() {
   if (click != 0) {
     return 0
   }
-  
-  click ++
+
+  click++
   var mensagemPadrao = document.querySelector('.mensagem-1')
   var cLoader = document.querySelector('.c-loader')
   var mensagemConcluido = document.querySelector('.concluido')
@@ -34,16 +49,16 @@ function enviarMensagem(){
   cLoader.classList.remove('ocultar')
   cLoader.classList.add('aparece')
 
-  setTimeout(function(){
+  setTimeout(function () {
     cLoader.classList.remove('aparece')
     cLoader.classList.add('ocultar')
   }, 3000)
 
-  setTimeout(function(){
+  setTimeout(function () {
     mensagemConcluido.classList.remove('ocultar')
     mensagemConcluido.classList.add('aparece')
   }, 3000)
-  setTimeout(function(){
+  setTimeout(function () {
     mensagemConcluido.classList.remove('aparece')
     mensagemConcluido.classList.add('ocultar')
     mensagemPadrao.classList.remove('ocultar')
